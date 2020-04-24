f77f9b6fd4909830018dc5f990e1c5b22529ffb0
Arash Bazrafshan <abazrafshan123@gmail.com>
2020-04-23 20:26:35 -0700

    eliminated display of separator commas between each value of generated password

--- script.js
+++ script.js
@@ -7,26 +7,24 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
   // console.log(newArr);
   passwordText.value = password;
-
 }
 
+//Array variable declarations that will be munipulated to yield an array of randomized values based on user preference when running this program
 var passwordArray = [];
 var newArr = [];
 var availableOptions = [];
-var lowerCase;
-var upperCase;
-var symbol;
-var number;
+//Arrays of the possible characters that may be used in password generation 
 var upperCaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var lowerCaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var symbolArray = [",","!","#","$","%","&","(",")","*","+","-",".","/",":",";","<","=",">","?","@","[","]","^","_","{","|","}","~"];
 var numberArray = ["1","2","3","4","5","6","7","8","9"];
+//user is asked to specify length of password, this value is stored and re-used for each password generation until user reloads page and restarts the program
 var passwordLength = prompt("How many characters would you like your password to be? Minimum: 8 characters; Maximum; 128 characters");
 
+//If user selects password length to be between 8 and 128 characters (inclusive) then next function is called, otherwise program ends and user can try again by reloading page
 function letsBegin() {
   console.log(passwordLength);
   if (Math.floor(passwordLength) >= 8 && Math.floor(passwordLength) <= 128) {
-    // upperCaseFunction();
     userPreference();
   }
   else {
@@ -35,11 +33,13 @@ function letsBegin() {
   }
 }
 
+// Prompts user for confirmation of which families of characters to include in password generation
 function userPreference(){
   var upperCase = confirm("Do you want to include uppercase letters in your password?"); 
   var lowerCase = confirm("Do you want to include lowercase letters in your password?");
   var number = confirm("Do you want to include numbers in your password?");
   var symbol = confirm("Do you want to include symbols in your password?");
+  // An array of characters that will be available for use in the generation of the password is created based on the user's preferences
   if (upperCase) {
     availableOptions.push(upperCaseArray);
     console.log (availableOptions);
@@ -57,6 +57,7 @@ function userPreference(){
     console.log(availableOptions);
     
   }
+  // If user doesn't select any of the prompted conditions to be true
   if (symbol === false && number === false && lowerCase === false && upperCase === false){
     alert("There must be SOME character types you want to include in your password. Reload page to try again!");
     return;
@@ -83,7 +84,7 @@ function generatePassword(arr)
     console.log(newArr[randomCharacterIndex])
     passwordArray.push(newArr[randomCharacterIndex]);
   }
-  return passwordArray;
+  return passwordArray.join(" ");
 }
 
 function start() {
